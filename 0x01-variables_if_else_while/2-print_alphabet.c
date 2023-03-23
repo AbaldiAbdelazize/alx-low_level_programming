662b1db129db6c040dfc55a42837eda4985278ae
root <abaldiabdelazize@gmail.com>
2023-03-23 16:12:43 -0700

    print alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,27 +1,13 @@
 #include <stdio.h>
-
 /**
- * main - print numbers from 00 to 99.
- *
- * Return: 0 on success
+ * main - Entry point
+ * Return: 0.
  */
 int main(void)
 {
-    int i;
-
-    for (i = 0; i < 100; i++)
-    {
-        putchar(i / 10 + '0');
-        putchar(i % 10 + '0');
-
-        if (i < 99)
-        {
-            putchar(',');
-            putchar(' ');
-        }
-    }
-
-    putchar('\n');
-
-    return (0);
+char abc;
+for (abc = 'a'; abc <= 'z'; abc++)
+putchar(abc);
+putchar('\n');
+return (0);
 }
