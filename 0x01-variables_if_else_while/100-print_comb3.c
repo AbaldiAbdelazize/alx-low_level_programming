8712b5cbe94cb61cd0e6de22ceeefa4d446ef918
root <abaldiabdelazize@gmail.com>
2023-03-23 16:29:03 -0700

    print comb3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,21 +1,36 @@
 #include <stdio.h>
 /**
-* main - Entry point
-* Return: 0.
-*/
+ * main - print numbers from 00 to 99.
+(*
+ * Return: 0 on success
+ */
 int main(void)
 {
-int num;
-for (num = 48; num <= 57; num++)
-{
-putchar(num);
-if (num != 57)
-{
-putchar(',');
-putchar(' ');
-}
-}
-putchar('\n');
-return (0);
-}
+	int i = '0';
+	int j = '0';
 
+	while (i <= '9')
+	{
+		while (j <= '9')
+		{
+			if (!(i > j || i == j))
+			{
+				putchar(i);
+				putchar(j);
+				if (i == '8' && j == '9')
+				{
+					putchar('\n');
+				}
+				else
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			j++;
+		}
+		j = '0';
+		i++;
+	}
+	return (0);
+}
