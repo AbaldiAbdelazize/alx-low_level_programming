5265d662707e8008a249dde2ccc8c904cfb71d17
root <abaldiabdelazize@gmail.com>
2023-03-29 16:02:02 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,22 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
-* main - Entry point
-*
-* Description: Prints Holberton, followed by a new line
-* Return: 0
- */
-
+  * main - Entry point
+  *
+  * Return: Always 0
+  */
 int main(void)
 {
-	char holberton[] = "Holberton";
-	int msg;
-	for (msg = 0; msg <= 8; msg++)
-	{
-		_putchar(holberton[msg]);
-	}
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
 	_putchar('\n');
+
 	return (0);
 }
